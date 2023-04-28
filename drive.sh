8d9945ee96fb13ce9e961261f6e9a5049597769c
bgopl092 <125370777+bgopl092@users.noreply.github.com>
2023-04-28 09:25:53 +0000

    Fri, Apr 28, 2023, 9:25 AM +00:00

--- drive.sh
+++ drive.sh
@@ -1,1 +1,1 @@
-wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.185-2/virtio-win-0.1.185.iso'
+wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso'
