0782b5cd48a98ac8a0c69abf8e2c299bc1508b13
bgopl092 <125370777+bgopl092@users.noreply.github.com>
2023-05-11 16:20:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,52 +1,5 @@
-#!/bin/bash
-echo 'Setup'
-wget -O firefox.deb 'http://security.ubuntu.com/ubuntu/pool/main/f/firefox/firefox_112.0+build2-0ubuntu0.18.04.1_amd64.deb'
-wget -O crd.deb'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'
-sleep 1
-clear 
-sleep 2
-echo 'Install'
-echo 'Update and Upgrade '
 sudo apt update -y ; sudo apt upgrade -y
-echo 'Install XFCE '
-sudo apt install xfce4 xfce4-goodies -y
-
-
-
-
-
-99
-
-1
-
-
-1
-
-sleep 1
-clear
-echo 'Install'
-echo '[-] Update and Upgrade'
-echo '[-] Install XFCE'
-echo 'Install Software'
-sudo apt install ./firefox.deb -y
-sudo apt install ./crd.deb -y
-sleep 1
-clear
-echo '[-] Install'
-echo '[-] Update and Upgrade'
-echo '[-] Install XFCE'
-echo '[-] Install Software'
-echo 'ChromeRemoteDesktop Setup'
-echo 'CODE CRD: https://remotedesktop.google.com/headless'
-echo 'Code Commnad:' read $CRD
-$CRD
-sudo apt autoremove -y
-sleep 1
-clear
-echo '[-] Install'
-echo '[-] Update and Upgrade'
-echo '[-] Install XFCE'
-echo '[-] Install Software'
-echo 'Setup'
-rm -rfv firefox.deb crd.deb > /dev/null 2>&1
-sudo apt autoremove -y 
+sudo apt install qemu qemu-utils qemu-system-x86-xen -y
+chmod +x qemu qemu-disk qemu-install dr 
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+ngrok config add-authtoken 1ubnr5orpjOPvJeMxOpeXOI27Od_ZMi97BL8r1D7nRWEM4t8
